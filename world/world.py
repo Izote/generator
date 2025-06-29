97101f3d277286c7f53064655fbc1b6bd65ff163
Mike <munguiamv@gmail.com>
2025-06-29 10:03:39 -0400

    basic array output

--- world/world.py
+++ world/world.py
@@ -1,3 +1,30 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from tcod.noise import Noise, grid
+
+if TYPE_CHECKING:
+    from numpy import ndarray
+
+
 class World:
-    def __init__(self) -> None:
-        pass
+    """A simulated World.
+
+    Parameters
+    ----------
+    shape: tuple[int]
+    
+    """
+    def __init__(self, shape: tuple[int, int]) -> None:
+        self.__shape = shape
+        self.map = self.__generate_noise()
+    
+    def __generate_noise(self) -> ndarray:
+        generator = Noise(dimensions=2)
+        
+        return generator[grid(shape=self.__shape, scale=0.25, origin=(2, 0))]
+
+    
+    @property
+    def shape(self) -> tuple[int, int]:
+        """Read-only access to the instance's `shape` attribute."""
+        return self.__shape
